d9963cec1c249dc53b231175fa9b84b002b9be77
KingRichardpt45 <ricfrancvi@gmail.com>
2022-12-22 22:36:46 +0000

    Update genes main

--- POO2/src/main/java/com/mycompany/poo2/Simulador.java
+++ POO2/src/main/java/com/mycompany/poo2/Simulador.java
@@ -13,70 +13,7 @@ public class Simulador {
 
     public static void main(String[] args) {
         
-        
-        System.out.println("teste");
-        
-        Gene a = new GeneEspecie("rosnar","geneEspecie");
-        Gene b = new GeneEspecifico("carinhoso","geneEspecifico", CararcteristicasEspecificas.OLHOS );
-        Gene c = new GeneDieta(Dieta.CARNIVORO,"geneDieta");
-        Gene d = new GeneLongividade(10,"geneLongitividade");
-        Gene e = new GeneRepoducao(30,"geneRepoducao");
-        Gene f = new GeneSexo(Sexo.FEMEA ,"geneSexo");
-        Gene g = new GeneAmbiente(Ambiente.DESETICO,"geneAmbiente");
-        Gene h = new GeneAtratividade(50,"geneAtratividade");
-        
-        /*System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);*/        
-        
-        FormatedString fstr_a = a.toFormatedString();
-        FormatedString fstr_b = b.toFormatedString();
-        FormatedString fstr_c = c.toFormatedString();
-        FormatedString fstr_d = d.toFormatedString();
-        FormatedString fstr_e = e.toFormatedString();
-        FormatedString fstr_f = f.toFormatedString();
-        FormatedString fstr_g = g.toFormatedString();
-        FormatedString fstr_h = h.toFormatedString();
-        
-        System.out.println(a);
-        System.out.println(fstr_a.getString() );
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(b);
-        System.out.println(fstr_b.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(c);
-        System.out.println(fstr_c.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(d);
-        System.out.println(fstr_d.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(e);
-        System.out.println(fstr_e.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(f);
-        System.out.println(fstr_f.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(g);
-        System.out.println(fstr_g.getString());
-        System.out.println("---------------------------------------------");
-        
-        System.out.println(h);
-        System.out.println(fstr_h.getString());
-        
-        System.out.println("------------------------------------------------------------------------------------------");
-        
-        
+       
     }
     
     
