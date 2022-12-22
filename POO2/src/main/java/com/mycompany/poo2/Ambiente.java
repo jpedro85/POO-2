99cf17d19fbfc228fec8e585926145a88bebeafe
KingRichardpt45 <ricfrancvi@gmail.com>
2022-12-22 13:58:19 +0000

    enums

---
+++ POO2/src/main/java/com/mycompany/poo2/Ambiente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.poo2;
+
+/**
+ *
+ * @author Francisco MSI
+ */
+public enum Ambiente {
+    
+    DESETICO,ROCHOSO,SAVANA,SELVA
+    
+}
---
+++ POO2/src/main/java/com/mycompany/poo2/CararcteristicasEspecificas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.poo2;
+
+/**
+ *
+ * @author Francisco MSI
+ */
+public enum CararcteristicasEspecificas {
+    
+    a("a");
+    
+    private String name;
+    
+    CararcteristicasEspecificas(String nome){
+        name = nome;
+    }
+   
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+}
