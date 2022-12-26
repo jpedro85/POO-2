af7e7deb3ef45e94d1e225ea1df4588404f4e09e
PedroFerreira11 <pedroferreira0620@gmail.com>
2022-12-26 23:31:47 +0000

    Update Zoo,Carregador,Ambiente,RepresentacaoInvalidaDoTipo e dos ficheiros txt
    updates

--- POO2/src/main/java/com/mycompany/poo2/Carregador.java
+++ POO2/src/main/java/com/mycompany/poo2/Carregador.java
@@ -17,24 +17,28 @@ public final class Carregador {
     private Carregador() {
     }
 
-    public <T> void carrega(Class<T> cclass, String ficheiro) {
+    public static <T> void carrega(Class<T> cclass, String ficheiro) {
         try {
             
             FileReader rW = new FileReader(ficheiro);
             BufferedReader bW = new BufferedReader(rW);
             String linha="";
+            linha=bW.readLine();
+            int contador=0;
             while(linha!=null) {
-                
+                contador++;
                 try {
 
-                    T a = cclass.getConstructor(FormatedString.class).newInstance(new FormatedString(ficheiro));
+                    T a = cclass.getConstructor(FormatedString.class).newInstance(new FormatedString(linha));
                     Registo<T> b = (Registo<T>) a;
                     b.addInstanceAoResgisto();
 
                 } catch (RepresentacaoInvalidaDoTipo exp1) {
-                    System.out.println(exp1.getMessage());
-                } catch (Exception expall) {
-                    System.out.println("MERDA!!!!!");
+                    System.out.println(exp1);
+                } catch (Exception expall) {//porque usamos a class construct nao é apanhada a exceccao representacaodotipo do construtor da class:Illegal access
+                    System.out.print("Item não carregado!! ");
+                    System.out.println("Na linha "+contador+" do ficheiro");
+                    System.out.println(RepresentacaoInvalidaDoTipo.getUltimaExcecao().getMessage());
                 }
                 linha=bW.readLine();
             }
--- POO2/src/main/java/com/mycompany/poo2/Simulador.java
+++ POO2/src/main/java/com/mycompany/poo2/Simulador.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.poo2;
 
 /**
@@ -11,5 +10,54 @@ package com.mycompany.poo2;
  */
 public class Simulador {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Carregador.carrega(GeneEspecie.class, "SimuladorSaves/TodosGenesDeEspecie.txt");
+        for (GeneEspecie gene : GeneEspecie.getAllInstances()) {
+            System.out.println(gene);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(Especie.class, "SimuladorSaves/TodosAsEspecies.txt");
+        for (Especie gene2 : Especie.getAllInstances()) {
+            System.out.println(gene2);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneAmbiente.class, "SimuladorSaves/TodosGenesDeAmbiente.txt");
+        for (GeneAmbiente gene3 : GeneAmbiente.getAllInstances()) {
+            System.out.println(gene3);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneAtratividade.class, "SimuladorSaves/TodosGenesDeAtratividade.txt");
+        for (GeneAtratividade gene4 : GeneAtratividade.getAllInstances()) {
+            System.out.println(gene4);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneDieta.class, "SimuladorSaves/TodosGenesDeDieta.txt");
+        for (GeneDieta gene5 : GeneDieta.getAllInstances()) {
+            System.out.println(gene5);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneEspecifico.class, "SimuladorSaves/TodosGenesDeEspecifico.txt");
+        for (GeneEspecifico gene6 : GeneEspecifico.getAllInstances()) {
+            System.out.println(gene6);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneLongividade.class, "SimuladorSaves/TodosGenesDeLongitividade.txt");
+        for (GeneLongividade gene7 : GeneLongividade.getAllInstances()) {
+            System.out.println(gene7);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneRepoducao.class, "SimuladorSaves/TodosGenesDeReproducao.txt");
+        for (GeneRepoducao gene8 : GeneRepoducao.getAllInstances()) {
+            System.out.println(gene8);
+        }
+        System.out.println("-----------------------------------------");
+        Carregador.carrega(GeneSexo.class, "SimuladorSaves/TodosGenesDeSexo.txt");
+        for (GeneSexo gene9 : GeneSexo.getAllInstances()) {
+            System.out.println(gene9);
+        }
+        System.out.println("-----------------------------------------");
+        
+        
+        
+    }
 }
