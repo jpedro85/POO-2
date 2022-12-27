f56959c076c9ca9034271f645e5ac9d6a0691892
KingRichardpt45 <ricfrancvi@gmail.com>
2022-12-27 16:12:20 +0000

    a

--- POO2/src/main/java/com/mycompany/poo2/Simulador.java
+++ POO2/src/main/java/com/mycompany/poo2/Simulador.java
@@ -1,14 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poo2;
 
 /**
  *
- * @author Pedro
+ * @author Francisco MSI
  */
 public class Simulador {
-
-    public static void main(String[] args) {}
+    
 }
