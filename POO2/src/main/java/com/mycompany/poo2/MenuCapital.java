4c80551e06783167d9b91aa37aa6f9d0b89581db
KingRichardpt45 <ricfrancvi@gmail.com>
2022-12-27 20:24:10 +0000

    updates Menuss

--- POO2/src/main/java/com/mycompany/poo2/MenuCapital.java
+++ POO2/src/main/java/com/mycompany/poo2/MenuCapital.java
@@ -9,5 +9,20 @@ package com.mycompany.poo2;
  * @author Pedro
  */
 public class MenuCapital extends Menu {
+
+    @Override
+    public void mostrarOpcoes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void excutarOpcaoPedida() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void executarOpcao(int optn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
